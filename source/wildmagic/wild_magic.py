207d55bf2b98c7a60f830f7d2aaa71862460b738
JPanes <joepanes@outlook.com>
2021-07-09 16:14:26 +0100

    Update wild_magic.py
    Now reads in csv file and stores it in a Dictionary.

--- source/wildmagic/wild_magic.py
+++ source/wildmagic/wild_magic.py
@@ -1,7 +1,7 @@
 import csv
 
 """
-1. Import CSV file
+
 2. Be able to randomly select one of the effects
 3. Be able to find one of the effects by entering a number
 4. Have a feature that keeps track of the effects that have been triggered
@@ -12,3 +12,35 @@ import csv
         4.4.1 If still blank, then don't add it to the csv
 
 """
+SPREADSHEET_PATH = "../../spreadsheets/"
+CURRENT_TABLE = "WildMagicEffects"
+
+def readEffectsFile(fileName):
+    """
+    Read in a CSV file, and prepares the data to be in an easy and quickly referenceable format.
+
+    Worth noting, the presumption is that the csv file is in order and the numbers are positive.
+    If this is not the case, then either consider altering your input file, or change the logic used within this file.
+    
+    INPUT:
+        :param fileName: String, the name of the file
+
+    OUTPUTS:
+        returns, dictionary containing all effects and the number of the last effect
+
+    """ 
+    wildMagic = {}
+    effectNo = 0
+    with open(SPREADSHEET_PATH + fileName + ".csv", "r") as dataFile:
+        myReader = csv.DictReader(dataFile)
+
+        for row in myReader:
+            effectNo = int(row["number"])
+            wildMagic[effectNo] = row["effect"]
+
+    return wildMagic, effectNo
+
+wildTable, maxNo = readEffectsFile(CURRENT_TABLE)
+
+print(wildTable)
+print(maxNo)
