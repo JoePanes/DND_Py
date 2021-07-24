76e770427a7ad0565bd69f0f4c11d7e9e27edf6a
JPanes <joepanes@outlook.com>
2021-07-24 10:39:31 +0100

    Update main.py
    Cleaned up wildMagicMenu() so that it is more readable, moving the core of the  functionality for each option into their own function.

--- source/main.py
+++ source/main.py
@@ -4,6 +4,9 @@ import wildmagic.wild_magic as wildTable
 
 from wildmagic.wild_text_to_spreadsheet import converter
 
+"""
+1. Properly handle the situation when usingDefault = False
+"""
 
 def wildMagicMenu():
     """
@@ -15,7 +18,7 @@ def wildMagicMenu():
     OUTPUT:
         returns nothing
     """
-    targetEffectsDict = {
+    categoryConversion = {
         "o" : "objects",
         "c" : "characters",
         "a" : "areas",
@@ -29,10 +32,9 @@ def wildMagicMenu():
 
     inp = input("~ ")
 
+    #Preparing and getting the program ready
     if inp == "1":
         converter(False)
-    
-
 
     elif inp == "2":
         print("-Wild Magic Menu")
@@ -64,7 +66,8 @@ def wildMagicMenu():
         print("Reading spreadsheet...")
         effects, maxNo = wildTable.readEffectsFile(fileName, True)
         print("Successfully read spreadsheet...")
-    
+
+        #Preparation complete
         stop = False
         
         while stop == False:
@@ -76,161 +79,13 @@ def wildMagicMenu():
             
             inp = input("~ ")
             if inp == "1" or inp == "2":
-                effectNo = ""
-
                 if inp == "1":
-                    validInput = False
-                    
-                    #Get a valid number from the user
-                    while validInput == False:
-                        try:
-                            currNo = int(input(f"Enter a number in the range of 1 to {maxNo}: "))
-
-                            if currNo < 1 or currNo > maxNo:
-                                raise ValueError()
-                            else:
-                                validInput = True
-                                effectNo = currNo
-                        except:
-                            print("You did not enter a valid Integer, please try again")
+                    obtainEffect(False, effects, maxNo, categoryConversion, usingDefault)
                 else:
-                    effectNo = wildTable.getNumber(maxNo)
-
-                print("-----------------")
-                effectText = wildTable.getEffect(effects, maxNo, effectNo)
-
-                categoryOfEffect, categoryOfEffectInteger = wildTable.getCategory(effectNo, True)
-
-                print(f"Type of Effect: {categoryOfEffect}")
-
-                print(f"{effectNo} - {effectText}")
-                print("-----------------")
-                print("Would you like to save the result? [y]/n")
-
-                inp = input("~ ")
-
-                if inp.lower() != "n":
-                    noIterations = 1
-                    targetOfEffect = ""
-
-                    #Based upon which category is in effect, it will require different manners of book keeping
-                    if usingDefault == True and (categoryOfEffectInteger == 1 or categoryOfEffectInteger == 2):
-                        print("Who is the effect being applied to?")
-                        print("(C)haracter")
-                        print("(O)bject (e.g. a building, chair, etc.)")
-                        validInput = False
-                        while validInput != True:
-                            inp = input("~ ")
-                        
-                            if inp.lower() == "c" or inp.lower() == "o": 
-                                validInput = True
-                                targetOfEffect = targetEffectsDict[inp]
-                            else:
-                                print("Invalid input, please try again")
-                            
-                        
-
-                    elif usingDefault == False or categoryOfEffectInteger == 3:
-                        print("Does the effect specify effecting specific people (not a general area or object)?")
-                        print("Type either:")
-                        print("(A)rea - Where it effects the world, settlement, or region in some way.")
-                        print("(O)bject - Where it effects an object (e.g. the next weapon to do...)")
-                        print("(C)haracter - Where it impacts one or more characters")
-
-                        targetOfEffect = ""
-
-                        inp = input("~ ")
-                        
-                        while inp.lower() not in targetEffectsDict:
-                            print("Invalid input, please try again")
-                            inp = input("~ ")
-
-                        targetOfEffect = targetEffectsDict[inp]
-
-                        
-                        if targetOfEffect.lower() == "characters" or targetOfEffect.lower() == "objects":
-                            print(f"How many noteworthy {targetOfEffect} affected by Wild Magic?")
-                            noIterations = ""
-
-                            while type(noIterations) != int:
-                                try:
-                                    noIterations = int(input("Enter number: "))
-                                
-                                except:
-                                    print("Invalid value, please enter any number")
-
-                    else:
-                        print("ERROR")
-                        print("An unexpected value has been recieved for the category of effect")
-                        print("If you have made changes, then you need to specify how to handle this category")
-                        
-                    for _ in range(noIterations):
-                        saveEffect(effectNo, effectText, targetOfEffect)
+                    obtainEffect(True, effects, maxNo, categoryConversion, usingDefault)
                                
             elif inp == "3":
-                print("Which would you like to access?")
-                print("(A)rea")
-                print("(O)bject")
-                print("(C)haracter")
-                
-                inp = input("~ ")
-
-                if inp.lower() in targetEffectsDict:
-                    folder = targetEffectsDict[inp.lower()]
-
-                    csvFiles = wildTable.getEffectedFiles(folder, True)
-
-                    if len(csvFiles) == 0:
-                        print("Sorry, but no effects have been saved for this category.")
-                    
-                    else:
-                        print("Within the category, the following files were found:")
-                        i = 1
-
-                        for currFile in csvFiles:
-                            print(f"{i}) {currFile}")
-                            i += 1
-
-                        print("Which one would you like to see?")
-                        print("Please enter the corresponding number")
-                        validInput = False
-
-                        while validInput != True:
-                            fileNumber = takeInAndConfirmUserInput()
-
-                            try:
-                                fileNumber = int(fileNumber)
-                                validInput = True
-                            except:
-                                print("The value that you entered, was not a number, please try again")
-                        
-                        #Adjust for list indexs
-                        fileNumber -= 1
-                        fileName = csvFiles[fileNumber]
-
-                        fileContents = wildTable.getEffectedCSV(folder, fileName, True)
-                        print("")
-                        for currRow in fileContents:
-                            #Format the number so that it remains a consistent size
-                            formattedNumber = len(str(maxNo)) *["0"]
-
-                            splitNumber = [number for number in currRow["number"]]
-
-                            i = 1
-                            for currNumber in splitNumber[::-1]:
-                                formattedNumber[-i] = currNumber
-                                i+=1
-
-                            formattedNumber = "".join(formattedNumber)    
-                            
-                            date = currRow["date&time"]
-                            effect = currRow["effect"]
-
-                            print(f"{formattedNumber} | {date} | {effect}")
-                        print("")
-
-                else:
-                    print("Invalid input, only the options listed above are valid")
+                getAppliedEffectsFromFile(maxNo, categoryConversion)
 
             elif inp == "4":
                 stop = True
@@ -295,7 +150,6 @@ def saveEffect(effectNo, effectText, targetOfEffect):
 
     wildTable.saveOutcome(targetName, targetOfEffect, effectNo, effectText, True)
 
-
 def takeInAndConfirmUserInput():
     """
     After taking in input from a user, confirm whether there are no mistakes
@@ -319,6 +173,190 @@ def takeInAndConfirmUserInput():
             validInput = True
 
     return userInput
+
+def obtainEffect(choseRandom, effects, maxNo, categoryConversion, usingDefault):
+    """
+    Based upon the user input, get an effect, then determine whether to save it to a .csv
+
+    INPUT:
+        :param choseRandom: Boolean, whether the user wishes to randomly get an effect or select one specifically
+        :param effects: List of Strings, the entire effects table
+        :param maxNo: Integer, the last number within the magic table
+        :param categoryConversion: Dictionary, used to convert single characters into their full names
+        :param usingDefault: Boolean, whether the normal effects table is being used
+    
+    OUTPUT:
+        returns nothing, but prints to the command line and can save to a .csv
+
+    """    
+
+    effectNo = ""
+
+    if choseRandom:
+        effectNo = wildTable.getNumber(maxNo)
+    
+    else:
+        validInput = False
+        
+        #Get a valid number from the user
+        while validInput == False:
+            try:
+                currNo = int(input(f"Enter a number in the range of 1 to {maxNo}: "))
+
+                if currNo < 1 or currNo > maxNo:
+                    raise ValueError()
+                else:
+                    validInput = True
+                    effectNo = currNo
+            except:
+                print("You did not enter a valid Integer, please try again")
+        
+
+    print("-----------------")
+    effectText = wildTable.getEffect(effects, maxNo, effectNo)
+
+    categoryOfEffect, categoryOfEffectInteger = wildTable.getCategory(effectNo, True)
+
+    print(f"Type of Effect: {categoryOfEffect}")
+
+    print(f"{effectNo} - {effectText}")
+    print("-----------------")
+    print("Would you like to save the result? [y]/n")
+
+    inp = input("~ ")
+
+    if inp.lower() != "n":
+        noIterations = 1
+        targetOfEffect = ""
+
+        #Based upon which category is in effect, it will require different manners of book keeping
+        if usingDefault == True and (categoryOfEffectInteger == 1 or categoryOfEffectInteger == 2):
+            print("Who is the effect being applied to?")
+            print("(C)haracter")
+            print("(O)bject (e.g. a building, chair, etc.)")
+            validInput = False
+            while validInput != True:
+                inp = input("~ ")
+            
+                if inp.lower() == "c" or inp.lower() == "o": 
+                    validInput = True
+                    targetOfEffect = categoryConversion[inp]
+                else:
+                    print("Invalid input, please try again")
+                
+            
+
+        elif usingDefault == False or categoryOfEffectInteger == 3:
+            print("Does the effect specify effecting specific people (not a general area or object)?")
+            print("Type either:")
+            print("(A)rea - Where it effects the world, settlement, or region in some way.")
+            print("(O)bject - Where it effects an object (e.g. the next weapon to do...)")
+            print("(C)haracter - Where it impacts one or more characters")
+
+            targetOfEffect = ""
+
+            inp = input("~ ")
+            
+            while inp.lower() not in categoryConversion:
+                print("Invalid input, please try again")
+                inp = input("~ ")
+
+            targetOfEffect = categoryConversion[inp]
+
+            
+            if targetOfEffect.lower() == "characters" or targetOfEffect.lower() == "objects":
+                print(f"How many noteworthy {targetOfEffect} affected by Wild Magic?")
+                noIterations = ""
+
+                while type(noIterations) != int:
+                    try:
+                        noIterations = int(input("Enter number: "))
+                    
+                    except:
+                        print("Invalid value, please enter any number")
+
+        else:
+            print("ERROR")
+            print("An unexpected value has been recieved for the category of effect")
+            print("If you have made changes, then you need to specify how to handle this category")
+            
+        for _ in range(noIterations):
+            saveEffect(effectNo, effectText, targetOfEffect)
+
+def getAppliedEffectsFromFile(maxNo, categoryConversion):
+    """
+    Get the name and location of a .csv file, then display all effects in an easy to read manner
+
+    INPUT:
+        :param maxNo: Integer, the last number within the magic table
+        :param categoryConversion: Dictionary, used to convert single characters into their full names
+    
+    OUTPUT:
+        returns nothing, but prints the contents of a .csv file to the command line
+    """
+    print("Which would you like to access?")
+    print("(A)rea")
+    print("(O)bject")
+    print("(C)haracter")
+
+    inp = input("~ ")
+
+    if inp.lower() in categoryConversion:
+        folder = categoryConversion[inp.lower()]
+
+        csvFiles = wildTable.getEffectedFiles(folder, True)
+
+        if len(csvFiles) == 0:
+            print("Sorry, but no effects have been saved for this category.")
+
+        else:
+            print("Within the category, the following files were found:")
+            i = 1
+
+            for currFile in csvFiles:
+                print(f"{i}) {currFile}")
+                i += 1
+
+            print("Which one would you like to see?")
+            print("Please enter the corresponding number")
+            validInput = False
+
+            while validInput != True:
+                fileNumber = takeInAndConfirmUserInput()
+
+                try:
+                    fileNumber = int(fileNumber)
+                    validInput = True
+                except:
+                    print("The value that you entered, was not a number, please try again")
+            
+            #Adjust for list indexs
+            fileNumber -= 1
+            fileName = csvFiles[fileNumber]
+
+            fileContents = wildTable.getEffectedCSV(folder, fileName, True)
+            print("")
+            for currRow in fileContents:
+                #Format the number so that it remains a consistent size
+                formattedNumber = len(str(maxNo)) *["0"]
+                splitNumber = [number for number in currRow["number"]]
+
+                i = 1
+                for currNumber in splitNumber[::-1]:
+                    formattedNumber[-i] = currNumber
+                    i+=1
+
+                formattedNumber = "".join(formattedNumber)    
+                
+                date = currRow["date&time"]
+                effect = currRow["effect"]
+
+                print(f"{formattedNumber} | {date} | {effect}")
+            print("")
+
+    else:
+        print("Invalid input, only the options listed above are valid")
+
 
 print("Hello, and welcome to my D&D program!")
 print("Please enter the bracketed character to select:")
